62770b48f163826cd04c30ae284cb418535ef0dd
John Wason <wason@wasontech.com>
2022-12-31 03:32:16 -0500

    Use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 from setuptools import setup, find_packages, find_namespace_packages
 
 setup(
-    name='pyri-robotics-motion-program-browser',
-    version='0.1.0',
-    description='PyRI Teach Pendant WebUI Browser Robotics Motion Program',
-    author='John Wason',
-    author_email='wason@wasontech.com',
-    url='http://pyri.tech',
     package_dir={'': 'src'},
     packages=find_namespace_packages(where='src'),
     include_package_data=True,
@@ -14,14 +8,5 @@ setup(
         'pyri.robotics_motion_program_browser.panels': ['*.html'],
         'pyri.robotics_motion_program_browser.components': ['*.html']
     },
-    zip_safe=False,
-    install_requires=[
-        'pyri-common',        
-        'importlib-resources',        
-    ],
-    entry_points = {
-        'pyri.plugins.webui_browser_panel': ['pyri-robotics-motion-program-browser=pyri.robotics_motion_program_browser.panels.robotics_mp_panels:get_webui_browser_panel_factory'],
-        'pyri.plugins.webui_browser_component': ['pyri-robotics-motion-program-browser=pyri.robotics_motion_program_browser.components.robotics_mp_components:get_webui_browser_component_factory'],
-        'pyri.plugins.webui_browser_plugin_init': ['pyri-robotics-motion-program-browser=pyri.robotics_motion_program_browser.robotics_mp_plugin_init:get_webui_browser_plugin_init_factory'],
-    }
+    zip_safe=False
 )
